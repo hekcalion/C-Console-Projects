72560df33b28e7c2327231bb3591384d477c55e0
DzurahIvan <ivandzurah@gmail.com>
2022-02-04 01:59:27 +0200

    Add logic to Program.cs

---
+++ Image Processing (Using Pointers)/CoreMatrix.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Image_Processing
+{
+    public sealed class CoreMatrix
+    {
+        public static double[,] EdgeDetection
+        {
+            get
+            {
+                return new double[,]
+                {
+                    { -1, -1, -1 },
+                    { -1, 8, -1 },
+                    { -1, -1, -1 }
+                };
+            }
+        }
+        public static double[,] Contrast
+        {
+            get
+            {
+                return new double[,]
+                {
+                    { 0, -1, 0 },
+                    { -1, 8, -1 },
+                    { 0, -1, 0 }
+                };
+            }
+        }
+        public static double[,] Vertical
+        {
+            get
+            {
+                return new double[,]
+                {
+                    { -1, 2, -1 },
+                    { -1, 2, -1 },
+                    { -1, 2, -1 }
+                };
+            }
+        }
+        public static double[,] Horizontal
+        {
+            get
+            {
+                return new double[,]
+                {
+                    { -1, -1, -1 },
+                    { 2, 2, 2 },
+                    { -1, -1, -1 }
+                };
+            }
+        }
+    }
+}
+
---
+++ Image Processing (Using Pointers)/ImageProcess.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Image_Processing
+{
+    public static class ImageProcess
+    {
+        public static Bitmap Convolution(double[,] convolutionCore, Bitmap image)
+        {
+            Bitmap tempImage = new Bitmap(image);
+            BitmapData tempImageData = tempImage.LockBits(new Rectangle(0, 0, tempImage.Width, tempImage.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            BitmapData imageData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            unsafe
+            {
+                byte* tempImagePtr = (byte*) tempImageData.Scan0.ToPointer();
+                byte* imagePtr = (byte*)imageData.Scan0.ToPointer();
+                int stride = tempImageData.Stride;
+                int height = image.Height - 1;
+                double sum = 0;
+                for(int i = 1; i< height; i++)
+                {
+                    byte* tempPos = tempImagePtr + (stride * i);
+                    byte* imgPos = imagePtr + (stride * i);
+                    for (int j= 0; j< stride; j++)
+                    {
+                        if (j > 2 && j < stride-3)
+                        {
+                            sum += (convolutionCore[0, 0] * (*(tempPos - stride - 3)));
+                            sum += (convolutionCore[0, 1] * (*(tempPos - stride)));
+                            sum += (convolutionCore[0, 2] * (*(tempPos - stride + 3)));
+                            sum += (convolutionCore[1, 0] * (*(tempPos - 3)));
+                            sum += (convolutionCore[1, 1] * (*(tempPos)));
+                            sum += (convolutionCore[1, 2] * (*(tempPos + 3)));
+                            sum += (convolutionCore[2, 0] * (*(tempPos + stride - 3)));
+                            sum += (convolutionCore[2, 1] * (*(tempPos + stride)));
+                            sum += (convolutionCore[2, 2] * (*(tempPos + stride + 3)));
+                            if (sum > 255) sum = 255;
+                            if (sum < 0) sum = 0;
+                            *imgPos = (byte)sum;
+                        }
+                        sum = 0;
+                        tempPos++;
+                        imgPos++;
+                    }                   
+                }
+                image.UnlockBits(imageData);
+                tempImage.UnlockBits(tempImageData);
+                return image;
+            }
+        }
+
+        public static Bitmap DeleteRed(Bitmap image)
+        {
+            BitmapData imageData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            unsafe
+            {
+                byte* ptr = (byte*)(imageData.Scan0.ToPointer());
+                int stride = imageData.Stride;
+                int height = imageData.Height;               
+                for(int i= 0; i< height; i++)
+                {
+                    byte* position = ptr + stride * i;
+                    int step = 0;
+                    for (int j = 0; j < stride; j++)
+                    {
+                        if(j == step)
+                        {
+                            *position = (byte)(0);
+                            step += 3;
+                        }
+                        position++;
+                    }
+                }
+            }
+            image.UnlockBits(imageData);
+            return image;
+        }
+
+        public static Bitmap DeleteGreen(Bitmap image)
+        {
+            BitmapData imageData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            unsafe
+            {
+                byte* ptr = (byte*)(imageData.Scan0.ToPointer());
+                int stride = imageData.Stride;
+                int height = imageData.Height;
+                for (int i = 0; i < height; i++)
+                {
+                    byte* position = ptr + stride * i;
+                    int step = 2;
+                    for (int j = 0; j < stride; j++)
+                    {
+                        if (j == step)
+                        {
+                            *position = (byte)(0);
+                            step += 3;
+                        }
+                        position++;
+                    }
+                }
+            }
+            image.UnlockBits(imageData);
+            return image;
+        }
+
+        public static Bitmap DeleteBlue(Bitmap image)
+        {
+            BitmapData imageData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            unsafe
+            {
+                byte* ptr = (byte*)(imageData.Scan0.ToPointer());
+                int stride = imageData.Stride;
+                int height = imageData.Height;
+                for (int i = 0; i < height; i++)
+                {
+                    byte* position = ptr + stride * i;
+                    int step = 0;
+                    for (int j = 0; j < stride; j++)
+                    {
+                        if (j == step)
+                        {
+                            *position = (byte)(0);
+                            step += 3;
+                        }
+                        position++;
+                    }
+                }
+            }
+            image.UnlockBits(imageData);
+            return image;
+        }
+
+
+    }
+}
