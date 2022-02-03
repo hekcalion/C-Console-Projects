2783a024efb1b7082250bbc3d8b9714ea31cb7d7
DzurahIvan <ivandzurah@gmail.com>
2022-02-04 00:43:36 +0200

    Add logic to Program.cs
    Image filter wich used convolution. For access to each pixels used standart methods GetPixel() , SetPixel().

---
+++ Image Processing/ImageProcess.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Image_Processing
+{
+    public static class ImageProcess
+    {
+        public static Bitmap Convolution(double[,] convolutionCore, Bitmap image)
+        {
+            Bitmap result = new Bitmap(image.Width, image.Height);
+            Color pixelColor;
+            int row = convolutionCore.GetLength(0);
+            int col = convolutionCore.GetLength(1);
+            double Red = 0, Green = 0, Blue = 0;
+            for (int i = 1; i < image.Width-1; i++)
+            {
+                for (int j = 1; j < image.Height-1; j++)
+                {
+                    for (int i1 = 0, c = (j - 1); i1 < col; i1++, c++)
+                    {
+                        for (int j1 = 0, r = (i - 1); j1 < row; j1++, r++)
+                        {                                                     
+                                pixelColor = image.GetPixel(r, c);
+                                Red += convolutionCore[i1, j1] * pixelColor.R;
+                                Green += convolutionCore[i1, j1] * pixelColor.G;
+                                Blue += convolutionCore[i1, j1] * pixelColor.B;                           
+                        }
+                    }
+                    Red = Red > 255 ? Red = 255 : Red;
+                    Green = Green > 255 ? Green = 255 : Green;
+                    Blue = Blue > 255 ? Blue = 255 : Blue;
+                    Red = Red < 0 ? Red = 0 : Red;
+                    Green = Green < 0 ? Green = 0 : Green;
+                    Blue = Blue < 0 ? Blue = 0 : Blue;
+                    pixelColor = Color.FromArgb((int)Red, (int)Green, (int)Blue);
+                    result.SetPixel(i, j, pixelColor);
+                    Red = Green = Blue = 0;
+                }
+            }
+            return result;
+        }
+
+        public static Bitmap DeleteRed (Bitmap image)
+        {
+            Bitmap result = new Bitmap(image.Width, image.Height);
+            Color pixelColor;
+            for (int i = 0; i < image.Width; i++)
+            {
+                for(int j = 0; j< image.Height; j++)
+                {
+                    pixelColor = image.GetPixel(i, j);
+                    result.SetPixel(i, j, Color.FromArgb(0,pixelColor.G,pixelColor.B));
+                }
+            }
+            return result;
+        }
+
+        public static Bitmap DeleteGreen (Bitmap image)
+        {
+            Bitmap result = new Bitmap(image.Width, image.Height);
+            Color pixelColor;
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    pixelColor = image.GetPixel(i, j);
+                    result.SetPixel(i, j, Color.FromArgb(pixelColor.R, 0, pixelColor.B));
+                }
+            }
+            return result;
+        }
+
+        public static Bitmap DeleteBlue (Bitmap image)
+        {
+            Bitmap result = new Bitmap(image.Width, image.Height);
+            Color pixelColor;
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    pixelColor = image.GetPixel(i, j);
+                    result.SetPixel(i, j, Color.FromArgb(pixelColor.R, pixelColor.G, 0));
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Image Processing/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Image_Processing
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Bitmap Image;
+            Bitmap result;
+            string filterName;
+            Stopwatch timer = new Stopwatch();
+            Console.WriteLine("Print full path to image");
+            string pathToImage = Console.ReadLine();
+            Console.WriteLine();
+            bool fileExist = File.Exists(pathToImage);          
+            if(!fileExist)
+            {
+                int fakePath = 0;
+                int maxFaxePass = 3;
+                do
+                {
+                    if(fakePath == maxFaxePass)
+                    {
+                        Console.WriteLine("Do you want exit? (Yes/No)");
+                        string exitChoice = Console.ReadLine();
+                        Console.WriteLine();
+                        if( exitChoice.Equals("Yes") || exitChoice.Equals("yes"))
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            maxFaxePass*=2;
+                            continue;
+                        }
+                    }
+                    Console.WriteLine("File does not exist!");
+                    Console.WriteLine("Print new path to image");
+                    pathToImage = Console.ReadLine();
+                    Console.WriteLine();
+                    fakePath++;
+                } while (!File.Exists(pathToImage));
+            }
+            using (FileStream imageStream = new FileStream(pathToImage, FileMode.Open, FileAccess.Read))
+            {
+                Image = new Bitmap(imageStream);
+            }
+            bool correctNumber = true;
+            int filterChoice;
+            do
+            {
+                Console.WriteLine("Choose filter:");
+                Console.WriteLine("1- Edge detection");
+                Console.WriteLine("2- Contrast");
+                Console.WriteLine("3- Vertical");
+                Console.WriteLine("4- Horizontal");
+                Console.WriteLine("5- Delete Red Color");
+                Console.WriteLine("6- Delete Green Color");
+                Console.WriteLine("7- Delete Blue Color");
+                bool isNumber = int.TryParse(Console.ReadLine(), out filterChoice);
+                if (!isNumber)
+                {
+                    Console.WriteLine("Incorrect input! \n");
+                    continue;
+                }
+                else if (filterChoice < 0 || filterChoice > 7)
+                {
+                    Console.WriteLine("Incorrect number! \n");
+                    continue;
+                }             
+                correctNumber = false;               
+            } while (correctNumber);
+            switch (filterChoice)
+            {
+                case 1:
+                    filterName = nameof(CoreMatrix.EdgeDetection);
+                    timer.Start();
+                    result = ImageProcess.Convolution(CoreMatrix.EdgeDetection, Image);
+                    timer.Stop();
+                    break;
+                case 2:
+                    filterName = nameof(CoreMatrix.Contrast);
+                    timer.Start();
+                    result = ImageProcess.Convolution(CoreMatrix.Contrast, Image);
+                    timer.Stop();
+                    break;
+                case 3:
+                    filterName = nameof(CoreMatrix.Vertical);
+                    timer.Start();
+                    result = ImageProcess.Convolution(CoreMatrix.Vertical, Image);
+                    timer.Stop();
+                    break;
+                case 4:
+                    filterName = nameof(CoreMatrix.Horizontal);
+                    timer.Start();
+                    result = ImageProcess.Convolution(CoreMatrix.Horizontal, Image);
+                    timer.Stop();
+                    break;
+                case 5:
+                    filterName = nameof(ImageProcess.DeleteRed);
+                    timer.Start();
+                    result = ImageProcess.DeleteRed(Image);
+                    timer.Stop();
+                    break;
+                case 6:
+                    filterName = nameof(ImageProcess.DeleteGreen);
+                    timer.Start();
+                    result = ImageProcess.DeleteGreen(Image);
+                    timer.Stop();
+                    break;
+                case 7:
+                    filterName = nameof(ImageProcess.DeleteBlue);
+                    timer.Start();
+                    result = ImageProcess.DeleteBlue(Image);
+                    timer.Stop();
+                    break;
+                default:
+                    filterName = nameof(CoreMatrix.EdgeDetection);
+                    timer.Start();
+                    result = ImageProcess.Convolution(CoreMatrix.EdgeDetection, Image);
+                    timer.Stop();
+                    break;
+            }
+            Console.WriteLine("Image successfully filtered!");
+            Console.WriteLine($"Time of image filtering {(double)timer.ElapsedMilliseconds/1000} seconds");
+            result.Save($"{Path.GetDirectoryName(pathToImage)}\\{Path.GetFileNameWithoutExtension(pathToImage)}({filterName}).jpeg", ImageFormat.Jpeg);
+            Console.ReadKey();
+            return;
+        }
+    }
+}
